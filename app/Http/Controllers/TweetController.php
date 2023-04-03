bc87efc37b94aa5783211caa05358ca679b096e4
lennypaul <lenny@bobochicparis.com>
2023-04-03 18:23:32 +0200

    debug

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreTweetRequest;
-use App\Models\tweet;
+use App\Models\Tweet;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
